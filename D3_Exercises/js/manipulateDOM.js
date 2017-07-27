9e615ee1c04fd5299d32316b9d50ae90b96669b4
JimmyLuo <luojiajun@126.com>
2017-07-27 09:31:26 +0800

    Manipulate DOM in D3.

--- D3_Exercises/js/manipulateDOM.js
+++ D3_Exercises/js/manipulateDOM.js
@@ -1,6 +1,7 @@
 window.onload = function () {
 	var dataset = [ "I like dog", "I like cat", "I like snake"];
-	var li = d3.select("body").select("#fruits").selectAll("#apple");
+	var ul = d3.select("body").select("#fruits");
+	var li = ul.selectAll("#apple");
 
 	li.data(dataset);
 
@@ -9,4 +10,10 @@ window.onload = function () {
 	});
 
 	li.style({ "color": "red", "font-size": "36px"});
+
+	ul.append("li").text("Append li element");
+
+	ul.insert("li", ":first-child").text("Insert li element");
+
+	ul.select("#apple").remove();
 }
